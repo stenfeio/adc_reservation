41d607441f9bf1a2df7bcb852b92e6efa85b09bd
Talal <stenfeio@gmail.com>
2015-11-03 20:18:05 -0500

    fixed indentations

--- src/Concert.java
+++ src/Concert.java
@@ -1,26 +1,26 @@
-
-
 import java.util.Random;
 
-
 public class Concert implements Runnable {
-boolean[] ConcertDays= new boolean[10];
-Thread c;
-public Concert(){
-	c=new Thread(this);
-	c.start();
-}
-public void run(){
-	for(int i=0;i<10;i++){
-		  Random r= new Random();
-		  boolean c=r.nextBoolean();
-		  ConcertDays[i]=c;
-		  int j=i+1;
-		  if(ConcertDays[i]==false)
-			  System.out.print("Day"+j+"="+"NO-concert"+"\t");
-		  else
-			  System.out.print("Day"+j+"="+"Concert"+"\t");  
-	   }
-System.out.println();
-}
+
+	boolean[] ConcertDays= new boolean[10];
+	Thread c;
+
+    public Concert(){
+		c=new Thread(this);
+		c.start();
+	}
+
+    public void run(){
+		for(int i=0;i<10;i++){
+			  Random r= new Random();
+			  boolean c=r.nextBoolean();
+			  ConcertDays[i]=c;
+			  int j=i+1;
+			  if(ConcertDays[i]==false)
+				  System.out.print("Day"+j+"="+"NO-concert"+"\t");
+			  else
+				  System.out.print("Day"+j+"="+"Concert"+"\t");
+		   }
+		System.out.println();
+	}
 }
--- src/Rooms.java
+++ src/Rooms.java
@@ -1,6 +1,3 @@
-
-
-
 import java.util.Random;
 
 public class Rooms implements Runnable{
@@ -11,19 +8,20 @@ public Rooms(){
 	r=new Thread(this);
 	r.start();
 }
-public void run(){ 
-   for(int i=0;i<10;i++){
-	   int j=i+1;
-	  Random r= new Random();
-	  boolean c=r.nextBoolean();
-	  AvailRooms[i]=c;
-	  if(AvailRooms[i]==false)
-		  System.out.print("Day"+j+"="+"Rooms NOT-available"+"\t");
-	  else
-		  System.out.print("Day"+j+"="+"Rooms Available"+"\t");  
-	
-   }
-   System.out.println();
- }
+public void run(){
+	for(int i=0;i<10;i++){
+		int j=i+1;
+		Random r= new Random();
+		boolean c=r.nextBoolean();
+		AvailRooms[i]=c;
+
+	   	if(AvailRooms[i]==false)
+		  	System.out.print("Day"+j+"="+"Rooms NOT-available"+"\t");
+	   	else
+		  	System.out.print("Day"+j+"="+"Rooms Available"+"\t");
+
+    }
+	System.out.println();
+	}
 }
 
