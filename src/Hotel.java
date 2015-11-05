ab07671ce9d80f129c92d7e73904f192b96527c7
Urvashi <urvi.sharma2993@gmail.com>
2015-11-04 20:49:52 -0500

    Check the code if I am going right

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,7 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 
-public class Hotel {
+import Coordinator.Status;
+
+
+public class Hotel extends Thread {
     //TODO add instance variables saving the rooms here.
-	public Hotel(Rooms p1, Concert p2){
+	public int[] Rooms=new int[10];
+	//path to resources
+	 public enum Status{
+	        NORMAL, RECOVERY, FAILED
+	    }
+    final String resourcePath = "C:\\Users\\Talal\\IdeaProjects\\adc_reservation\\resources\\";
+    Status SYSTEM_STATUS;               //System status variable
+
+
+    File recoveryFile;                  //Defines recovery file path
+    BufferedWriter recoveryFileWriter;  //Defines recover file writer
+
+    File configurationFile;				//Defines hotel configuration path
+    BufferedReader configurationFileReader;    //Defines hotel configuration reader
+	/*public Hotel(Rooms p1, Concert p2){
 
         for(int i=0;i<10;i++){
 			int j=i+1;
@@ -9,5 +29,21 @@ public class Hotel {
 				System.out.println("Reservation can be done on day"+j);
 			}
 		}
+	}*/
+	void Initialize(){								// Initialize the no. of rooms as 8 for each day
+		for(int i=0;i<10;i++){                       
+			Rooms[i]=8;
+		}
+	}
+	
+	//Check if the reservation of the rooms can be done on particular day or not
+		void Reservation(int day){					
+			if(Rooms[day]!=0){
+				System.out.println("Reservation done");
+		Rooms[day]=Rooms[day]-1;
+			}
+			else
+				System.out.println("Room not available for this day");
 	}
 }
+
