bba41d016089c0a3dabbb7a7dfce4548bf604f5f
Urvashi <urvi.sharma2993@gmail.com>
2015-11-06 14:24:19 -0500

    Changes

--- src/HotelThread.java
+++ src/HotelThread.java
@@ -1,9 +1,11 @@
 	import java.io.BufferedReader;
-	import java.io.BufferedWriter;
-	import java.util.ArrayList;
-	import java.util.List;
-	import java.io.*;
-	import java.net.*;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+import java.net.*;
+
+
 	/**
 	 * This class serves as a grouping class for the threads that will run on the Hotel.
 
@@ -14,28 +16,28 @@
 	     * Contructor that takes in the bookingFileReader in order to read the booking requests
 	     * @param bookingFileReader
 	     */
-public class HotelThread {
 
 
 	 /**
      * Contructor that takes in the lists of Request in order to read the booking requests
      * @param bookingFileReader
      */
-	    public HotelThread(){
+	    public class HotelThread extends Thread{
 	      
 
-
-	    }
-
+	    
 	    /**
 	     * Private thread that handles opening the incoming socket
 	     */
-	    protected class IncomingThread extends Thread{
+	   
 	        //TODO open incoming socket
-	        @Override
-	        
+	    //    @Override
+	        Socket coordsocket;
+	        ObjectInputStream inStream=null;
+	        ObjectOutputStream outstream=null;
+	        Hotel h=new Hotel();
 	        public void run() {
-	        	 Socket coordsocket;
+	        	 
 	        	try
 	            {
 	     
@@ -48,33 +50,52 @@ public class HotelThread {
 	                {
 	                    //Reading the message from the client
 	                   coordsocket = serverSocket.accept();
-	                    InputStream is = coordsocket.getInputStream();
-	                    InputStreamReader isr = new InputStreamReader(is);
-	                    BufferedReader br = new BufferedReader(isr);
-	                    String object = br.readLine();
-	                    System.out.println("Message received from client is "+object);
+	                   inStream = new ObjectInputStream(coordsocket.getInputStream());
+	                   Request request = (Request) inStream.readObject();
+	                  //  InputStream is = coordsocket.getInputStream();
+	                  //  InputStreamReader isr = new InputStreamReader(is);
+	                   // BufferedReader br = new BufferedReader(isr);
+	                  //  String object = br.readLine();
+	                    System.out.println("Object recieved "+request);
+	                 
 	     
 	                    //Multiplying the number by 2 and forming the return message
 	                    String returnMessage;
 	                    try
-	                    {
-	                       // int numberInIntFormat = Integer.parseInt(object);
-	                        String returnValue = "object recieved";
+	                    {	//Check if the object is passed with the correct parameter or not
+	                    	if(request.id!=null ||request.dates!=null|| request.numberOfDays!=0 ||request.status!=null){
+	                       
+	                    	 Request request1= checkstatus(request);
+	                    	 String returnValue = "Success";
+		                        returnMessage = String.valueOf(returnValue) + "\n";
+	                    	 outstream = new ObjectOutputStream(coordsocket.getOutputStream());
+	                    	 outstream.writeObject(request1);
+	                    	
+	                    	
+	                    }	
+	                    else{
+	                    	String returnValue = "Recovery";
 	                        returnMessage = String.valueOf(returnValue) + "\n";
+	                        outstream = new ObjectOutputStream(coordsocket.getOutputStream());
+	                        outstream.writeObject(request);
 	                    }
-	                    catch(NumberFormatException e)
+	                    }
+	                    catch(Exception e)
 	                    {
-	                        //Input was not a number. Sending proper message back to client.
+	                       
 	                        returnMessage = "Please send a proper object\n";
 	                    }
-	     
+	                   // Request request1= checkstatus(request);  
 	                    //Sending the response back to the client.
-	                    OutputStream os = coordsocket.getOutputStream();
-	                    OutputStreamWriter osw = new OutputStreamWriter(os);
-	                    BufferedWriter bw = new BufferedWriter(osw);
-	                    bw.write(returnMessage);
+	                    
+	                    //outstream = new ObjectOutputStream(coordsocket.getOutputStream());
+	                  //  Request request= new Request();
+	                 //   outstream.writeObject(request1);
+	                   // OutputStreamWriter osw = new OutputStreamWriter(os);
+	                  //  BufferedWriter bw = new BufferedWriter(osw);
+	                  //  bw.write(returnMessage);
 	                    System.out.println("Message sent to the client is "+returnMessage);
-	                    bw.flush();
+	                  //  bw.flush();
 	                }
 	            }
 	            catch (Exception e)
@@ -90,14 +111,20 @@ public class HotelThread {
 	                catch(Exception e){}
 	            }
 	        }
-	    }
-
-	    protected class OutgoingThread extends Thread{
-	        @Override
-	        public void run() {
-
-	        }
-	    }
+	    
+	   Request checkstatus(Request request){
+		   for(int i=0;i<9;i++){
+			 
+		   if(h.Rooms[request.dates.get(i)]!=0)
+		   request.status=Request.RStatus.SUCCESS;
+		   else
+			   request.status=Request.RStatus.FAILED;
+		 
+		   
+		   }
+		   return request;
+	   }
+	   
 
 	    protected class FailRecoverThread extends Thread{
 	        @Override
@@ -112,50 +139,7 @@ public class HotelThread {
 	     * It should also handle forwarding the resquests to the outgoing
 	     * thread.
 	     */
-	    protected class OperationThread extends Thread{
-	        @Override
-	        public void run() {
-	            String tempRequest;         //temp string to read requests
-
-	            try {
-	                while ((tempRequest = bookingFileReader.readLine()) != null) {
-	                    requestArray.add(tempRequest);
-	                }
-	                System.out.println("Requests loaded into request list...");
-
-	                //closes the bookingFileReader after populating the request array
-	                bookingFileReader.close();
-
-	                //TODO parse the requests to make sense of them
-	                //converts string requests into Request objects
-	                for(int i = 0; i < requestArray.size(); i++) {
-	                    requestsList.add(parseRequest(requestArray.get(i)));
-	                    System.out.println(requestsList.get(i));
-	                }
-
-	            }catch(Exception e){
-	                System.err.println("Could not read from booking file in coordinator operation thread...");
-	                e.printStackTrace();
-	            }
-	        }
-
-	        /*
-	        Helper method whose job is to convert string requests to Request objects.
-	         */
-	        private Request parseRequest(String request){
-	            String[] tempStrings = request.replace("[", "").replace("]", "").split("\\s");
-	            String tempId = tempStrings[0];
-	            int tempNumberOfDays = Integer.parseInt(tempStrings[1]);
-	            List<Integer> dates = new ArrayList<>();
-
-	            for (int i = 2; i < tempStrings.length; i++){
-	                dates.add(Integer.parseInt(tempStrings[i]));
-	            }
-	            Request tempRequest = new Request(tempId, tempNumberOfDays, dates);
-
-	            return tempRequest;
-	        }
-	    }
+	   
 
 	
 
